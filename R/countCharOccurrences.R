6a07c5b707be6a254bd2448c29f485ffca24066b
AlejandroKantor <akantorb@gmail.com>
2016-11-19 19:08:38 -0500

    Added function countCharOccurrences

---
+++ R/countCharOccurrences.R
@@ -0,0 +1,20 @@
+#' Counts the number of times a single characte occurs.
+#'
+#' @description Counts the number of times a single characte occurs in each string of a character vector.
+#' @param char a single character cannot be a regular expression.
+#' @param v_s_string a character vector
+#' @param b_ignore_case a boolean default FALSE of we want to ignore the case
+#' @return a integer vector of the number of times the character occurs in each string in v_s_string.
+#'
+#' @examples
+#' countCharOccurrences("a", c("a other a", "zero"))
+#' countCharOccurrences("a", c("A sentence."))
+#' countCharOccurrences("a", c("A sentence."), b_ignore_case = T)
+#'
+countCharOccurrences <- function(char, v_s_string, b_ignore_case = FALSE) {
+  v_s_new_string <- gsub(char,"",v_s_string,ignore.case = b_ignore_case)
+  return (nchar(v_s_string) - nchar(v_s_new_string))
+}
+
+
+
---
+++ tests/testthat/test-countCharOccurrences.R
@@ -0,0 +1,9 @@
+context("Character Occurences")
+
+test_that("Correct count", {
+  expect_equal(countCharOccurrences("a", c("a other a", "zero")), c(2L, 0L))
+  expect_equal(countCharOccurrences("a", c("A sentence.")),0)
+  expect_equal(countCharOccurrences("a", c("A sentence."), b_ignore_case = T),1)
+
+
+})
