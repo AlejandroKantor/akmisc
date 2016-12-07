b17b9c9c4ce0aca2ac75d9282dfd72833e4c31bd
AlejandroKantor <akantorb@gmail.com>
2016-12-07 18:55:09 -0500

    Add option to cat by string intervals

--- R/CategorizationIntervals.R
+++ R/CategorizationIntervals.R
@@ -1,6 +1,7 @@
 #' Construction function for CategorizationIntervals class
 #' @description Construction function for CategorizationIntervals class used in \code{\link{categorizeByIntervals}}.
 #' @param value a vector.
+#' @param s_intervals a character vector. Each element must start with "[" or "(" and end with "]" or ")".
 #' @param min a numeric vector.
 #' @param max a numeric vector.
 #' @param close_left a integer vector with only values 0 or 1 or a logical vector.
@@ -17,8 +18,69 @@
 #'                                         close_left = c(0,1,0),
 #'                                         close_right = c(0,1,0) )
 #' print(ci_intervals)
-CategorizationIntervals <- function(value, min, max,  close_left, close_right, i_digits_label = 3){
-  # making sure each variable has the correct data type
+#' ci_intervals <- CategorizationIntervals(value = c(1,2,3),
+#'                                         s_intervals = c("(-Inf,0)","[0,0]","(0,Inf)"))
+#' print(ci_intervals)
+CategorizationIntervals <- function(value,
+                                    s_intervals = NULL,
+                                    min = NULL,
+                                    max = NULL,
+                                    close_left = NULL,
+                                    close_right = NULL,
+                                    i_digits_label = 3){
+
+  if( !is.null(s_intervals)){
+    ci_intervals <- extractCategoriesFromIntervals(value, s_intervals)
+  } else {
+    # making sure each variable has the correct data type
+    checkRepCategorizationIntervals( min, max, close_left, close_right)
+    ci_intervals <- data.table(value= value, min=min, max= max,
+                               close_left= close_left,close_right= close_right )
+  }
+  structure(ci_intervals, class = "CategorizationIntervals")
+  ci_intervals <- addLabelCategorizationIntervals(ci_intervals,i_digits = i_digits_label)
+  return(ci_intervals)
+}
+
+#' Extract Categories from Interals
+#' @description Parses a character vector of intervals to a data.table with numeric values
+#' @param value a vector.
+#' @param s_intervals a character vector. Each element must start with "[" or "(" and end with "]" or ")".
+#' @return data.table with columns value, min, max, close_left and close_right.
+#' @seealso \code{\link{categorizeByIntervals}}, \code{\link{CategorizationIntervals}}
+#' @examples
+#' dt_intervals <- extractCategoriesFromIntervals(value = c(1,2,3),
+#'                                         s_intervals = c("(-Inf,0)","[0,0]","(0,Inf)"))
+#' print(dt_intervals)
+extractCategoriesFromIntervals <- function(value, s_intervals){
+  dt_intervals <- data.table(value = value, intervals = s_intervals)
+  dt_intervals[  , close_left := ifelse(grepl("^\\[" ,intervals), 1,
+                                        ifelse(grepl("^\\(" ,intervals),0, NA_integer_)) ]
+  dt_intervals[  , close_right := ifelse(grepl("\\]$" ,intervals), 1,
+                                        ifelse(grepl("\\)$" ,intervals),0, NA_integer_)) ]
+  dt_intervals[ , intervals := gsub( "\\(|\\[|\\]|\\)", "", intervals)]
+  dt_intervals[ , min := as.numeric(gsub( ",.*" , "", intervals))]
+  dt_intervals[ , max := as.numeric(gsub( ".*," , "", intervals))]
+  dt_intervals[, intervals:= NULL]
+
+  setcolorder(dt_intervals, c("value", "min", "max", "close_left" , "close_right"))
+  return(dt_intervals)
+}
+
+
+#' Check rep of CategorizationIntervals
+#'
+#' @param min a numeric vector.
+#' @param max a numeric vector.
+#' @param close_left a integer vector with only values 0 or 1 or a logical vector.
+#' @param close_right a integer vector with only values 0 or 1 or a logical vector.
+#' @return NULL
+#' @keywords internal
+#'
+checkRepCategorizationIntervals <- function( min = NULL,
+                                             max = NULL,
+                                             close_left = NULL,
+                                             close_right = NULL){
   if( ! (is.numeric(max) & is.numeric(min) )){
     stop("max and min must be numeric")
   }
@@ -35,22 +97,16 @@ CategorizationIntervals <- function(value, min, max,  close_left, close_right, i
   }
 
   if(!( all(close_left %in% c(0,1)) & all(close_right %in% c(0,1)))){
-    stop("close_left and close_right can only ber TRUE FALSE 0 or 1")
+    stop("close_left and close_right can only be TRUE FALSE 0 or 1")
   }
 
-  ci_intervals <- data.table(value= value, min=min, max= max,
-                             close_left= close_left,close_right= close_right )
-  structure(ci_intervals, class = "CategorizationIntervals")
-  addLabelCategorizationIntervals(ci_intervals,i_digits = i_digits_label)
-  return(ci_intervals)
 }
-
 
 #' Adds Label to a CategorizationIntervals object in memory.
 #' @param ci_intervals a CategorizationIntervals object.
 #' @param i_digits number of digits in label. Default is 3.
 #' @return NULL
-#' @details Modifies ci_intervals object in memory. Is used as part of construction function \code{\link{CategorizationIntervals}}.
+#' @details Modifies ci_intervals object in memory also returns object. Is used as part of construction function \code{\link{CategorizationIntervals}}.
 
 addLabelCategorizationIntervals <- function(ci_intervals,i_digits = 3){
   ci_intervals[ , label_start:=ifelse( close_left==0, "(", "[")]
@@ -60,6 +116,7 @@ addLabelCategorizationIntervals <- function(ci_intervals,i_digits = 3){
 
   ci_intervals[, label_start:=NULL]
   ci_intervals[, label_end:=NULL]
+  return(ci_intervals)
 
 }
 
--- tests/testthat/test-categorizeByIntervals.R
+++ tests/testthat/test-categorizeByIntervals.R
@@ -20,14 +20,35 @@ test_that("differentTypesOfValues", {
 
 })
 
+
+
+test_that("differentTypesOfValuesLabels", {
+  ci_intervals <- CategorizationIntervals(value = c(1,2,3),
+                                          s_intervals = c("(-Inf,0)","[0,0]","(0,Inf)"))
+  v_n_values <- -3:3
+  v_expected <- c(1,1,1,2,3,3,3)
+  expect_equal(categorizeByIntervals(v_n_values, ci_intervals),v_expected)
+
+  ci_intervals[, value := c("a","b","c")]
+  v_expected <- c("a","a","a","b","c","c","c")
+  expect_equal(categorizeByIntervals(v_n_values, ci_intervals),v_expected)
+
+  ci_intervals[, value := factor(value)]
+  v_expected <- factor(c("a","a","a","b","c","c","c"))
+  expect_equal(categorizeByIntervals(v_n_values, ci_intervals),v_expected)
+
+})
+
+
 test_that("wierdIntervals", {
   ci_intervals <- CategorizationIntervals(value = c(-99,1,2,-99),
-                                          min = c(-Inf, 0, 0, 1.1),
-                                          max = c(0,    0, 1.1, Inf),
+                                          min = c(-Inf, 0, 0, 10/9),
+                                          max = c(0,    0, 10/9, Inf),
                                           close_left = c(0, 1 , 0, 0),
                                           close_right = c(0,1,1, 0) )
   v_n_values <- -3:3
   v_expected <- c(-99,-99,-99,1,2,-99,-99)
+  expect_equal(ci_intervals[value==2 , label] , "(0, 1.111]")
   expect_equal(categorizeByIntervals(v_n_values, ci_intervals),v_expected)
 
 
