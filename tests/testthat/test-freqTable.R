763b0b1fc9b34a35f12c8b1024fd82851f372456
AlejandroKantor <akantorb@gmail.com>
2017-01-30 19:55:37 -0500

    Add factor test

--- tests/testthat/test-freqTable.R
+++ tests/testthat/test-freqTable.R
@@ -29,3 +29,20 @@ test_that("frequencyTable", {
   expect_equal(dt_freq4[ 1 , perc], 12.5)
 
 })
+
+test_that("frequencyTableFactor", {
+  dt_data <- data.table(var1 = factor(c(1,1,2,2,3,3,3,3)),
+                        var2 = c(6,6,6,7,7,7,7,7))
+
+
+  dt_freq1 <- freqTable(dt_data, "var1", b_total_row = TRUE)
+  dt_freq2 <- freqTable(dt_data, c("var1","var2"), b_include_perc = TRUE)
+
+  dt_expected1 <- data.table(var1 = factor(c("1", "2", "3", "Total")),
+                             freq = c(2L,2L,4L,8L))
+  expect_identical(dt_freq1 , dt_expected1)
+  expect_equal(ncol(dt_freq2), 4)
+  expect_equal(dt_freq2[ var1 =="3" & var2 == 7 , perc], 50.0)
+
+
+})
